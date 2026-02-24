5cb6f3f72a711104f24e1b7290ea4c4e26c50265
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 16:25:06 +0900

    [처음부터 다시...] 임시로 잠깐

--- src/main/java/org/example/back/user/UserService.java
+++ src/main/java/org/example/back/user/UserService.java
@@ -1,14 +1,10 @@
 package org.example.back.user;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
 import org.example.back.user.model.AuthUserDetails;
 import org.example.back.user.model.EmailVerify;
 import org.example.back.user.model.User;
 import org.example.back.user.model.UserDto;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,65 +17,30 @@ import java.util.UUID;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final EmailVerifyRepository emailVerifyRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JavaMailSender mailSender;
+    private final EmailService emailService;
+    private final EmailVerifyRepository emailVerifyRepository;
 
-    public void signup(UserDto.SignupReq dto) {
+    public UserDto.SignupRes signup(UserDto.SignupReq dto) {
         User user = dto.toEntity();
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
         userRepository.save(user);
 
-        // 이메일 전송
-        MimeMessage message  = mailSender.createMimeMessage();
-        try {
-            String uuid = UUID.randomUUID().toString();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            helper.setTo(dto.getEmail());
-            String subject = "[안녕] 환영";
-            String htmlContents = "<a href='http://localhost:8080/user/verify?uuid="+uuid+"'>이메일 인증</a>";
-            helper.setSubject(subject);
-            helper.setText(htmlContents, true);
-            mailSender.send(message);
-            EmailVerify emailVerify = EmailVerify.builder().email(dto.getEmail()).uuid(uuid).build();
-            emailVerifyRepository.save(emailVerify);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        // 메일 전송
+        String uuid = UUID.randomUUID().toString();
+        emailService.sendWelcomeMail(uuid, dto.getEmail());
 
+        // 메일 전송 내역 저장
+        EmailVerify emailVerify = EmailVerify.builder().email(dto.getEmail()).uuid(uuid).build();
+        emailVerifyRepository.save(emailVerify);
 
-
+        return UserDto.SignupRes.from(user);
     }
 
-    public UserDetails login(UserDto.LoginReq dto) throws UsernameNotFoundException {
-        System.out.println("UserService 실행됨");
-
-        User user = userRepository.findByEmail(dto.getEmail()).orElseThrow();
-
-
-        if(passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
-        }
-
-        if(user.isEnable()) {
-        }
-
-
-
-        return AuthUserDetails.from(user);
-    }
-
-
-
-    // TODO : 5번
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("UserService 실행됨");
-
-        // TODO : 6번
         User user = userRepository.findByEmail(username).orElseThrow();
 
-
-        // TODO : 7번
         return AuthUserDetails.from(user);
     }
 
--- src/main/java/org/example/back/user/model/UserDto.java
+++ src/main/java/org/example/back/user/model/UserDto.java
@@ -16,7 +16,25 @@ public class UserDto {
                     .email(this.email)
                     .name(this.name)
                     .password(this.password)
+                    .enable(false)
                     .role("ROLE_USER")
+                    .build();
+        }
+    }
+
+
+    @Builder
+    @Getter
+    public static class SignupRes {
+        private Long idx;
+        private String email;
+        private String name;
+
+        public static SignupRes from(User entity) {
+            return SignupRes.builder()
+                    .idx(entity.getIdx())
+                    .email(entity.getEmail())
+                    .name(entity.getName())
                     .build();
         }
     }
