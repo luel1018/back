bc127c0aca306a8f84e40ffc1d2dadd873a5f3a7
김민g규 <luper1018@yonsei.ac.kr>
2026-02-24 09:27:39 +0900

    [??] 초기

---
+++ src/main/java/org/example/back/user/UserController.java
@@ -0,0 +1,38 @@
+package org.example.spring02.user;
+
+import lombok.RequiredArgsConstructor;
+import org.example.spring02.user.model.UserDto;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/user")
+@RestController
+@RequiredArgsConstructor    // TODO: Lombok 기능, final이 붙은 변수만 매개변수로 받는 생성자
+public class UserController {
+    private final UserService userService;
+
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody UserDto.SignupReq dto) {
+        userService.signup(dto);
+
+        return ResponseEntity.ok("성공");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserDto.LoginReq dto) {
+        try {
+            UserDto.LoginRes result = userService.login(dto);
+            return ResponseEntity.ok(result);
+        } catch (EmptyResultDataAccessException emptyException) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("실패");
+        }
+    }
+
+}
---
+++ src/main/java/org/example/back/user/model/User.java
@@ -0,0 +1,24 @@
+package org.example.spring02.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String email;
+    private String name;
+    private String password;
+}
