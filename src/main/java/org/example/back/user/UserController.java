79807e4a0d82bb3b344cbb0ad28cb770b499ed5b
luel1018 <luper1018@yonsei.ac.kr>
2026-02-24 16:25:15 +0900

    [처음부터 다시...] 임시로 잠깐

--- src/main/java/org/example/back/user/UserController.java
+++ src/main/java/org/example/back/user/UserController.java
@@ -1,16 +1,19 @@
 package org.example.back.user;
 
-import lombok.RequiredArgsConstructor;
 import org.example.back.user.model.AuthUserDetails;
 import org.example.back.user.model.UserDto;
 import org.example.back.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
+@CrossOrigin
 @RequestMapping("/user")
 @RestController
 @RequiredArgsConstructor
@@ -18,34 +21,34 @@ public class UserController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
 
-    @GetMapping("/verify")
-    public ResponseEntity verify(String uuid) {
-        userService.verify(uuid);
-        return ResponseEntity.ok("asd");
-    }
-
     @PostMapping("/signup")
     public ResponseEntity signup(@RequestBody UserDto.SignupReq dto) {
-        userService.signup(dto);
-        return ResponseEntity.ok("성공");
+        UserDto.SignupRes result =  userService.signup(dto);
+
+        return ResponseEntity.ok(result);
     }
+
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody UserDto.LoginReq dto) {
-        try {
-            UsernamePasswordAuthenticationToken token =
-                    new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword(), null);
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword(), null);
 
-            Authentication authentication = authenticationManager.authenticate(token);
-            AuthUserDetails user = (AuthUserDetails) authentication.getPrincipal();
+        Authentication authentication = authenticationManager.authenticate(token);
+        AuthUserDetails user = (AuthUserDetails) authentication.getPrincipal();
 
+        if(user != null) {
             String jwt = JwtUtil.createToken(user.getIdx(), user.getUsername(), user.getRole());
-            return ResponseEntity.ok()
-                    .header("Set-Cookie", "ATOKEN=" + jwt + "; Path=/")
-                    .build();
-
-        } catch (AuthenticationException e) {
-            return ResponseEntity.status(401).body("로그인 실패");
+            return ResponseEntity.ok().header("Set-Cookie", "ATOKEN=" + jwt + "; Path=/").build();
         }
+
+        return ResponseEntity.ok("로그인 실패");
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity verify(String uuid) {
+        userService.verify(uuid);
+        // 인증 성공하면 프론트로 리다이렉트
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).location(URI.create("http://localhost:5173")).build();
     }
 }
